36a93b10d1a632adef05faa7946923b1868ac88e
Dominik Korsa <dominik.korsa@gmail.com>
2021-02-13 12:38:34 +0100

    Move database config to env variables

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,5 +1,6 @@
 import type { Connection } from 'typeorm';
 import { createConnection } from 'typeorm';
+import { requireEnv } from '../utils';
 import Application from './entities/application';
 import Client from './entities/client';
 import Developer from './entities/developer';
@@ -12,9 +13,8 @@ class Database {
   public async connect(): Promise<void> {
     this.connection = await createConnection({
       type: 'mongodb',
-      host: 'localhost',
-      port: 27017,
-      database: 'wulkanowy-bridge-local',
+      url: requireEnv('DATABASE_URL'),
+      useNewUrlParser: true,
       entities: [
         Application,
         User,
